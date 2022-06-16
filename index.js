474a441d86853712f53cc758714119dda01c7823
jorge <jorgebarrera680@gmail.com>
2022-06-15 23:48:27 -0500

    changed font size on navbar

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your Code Here
+let username = window.prompt('please enter your name')
+
+let userscore = 0
+
+for(let i = 0; i < questions.length; i++){
+    let question = questions[i]
+    let useranswer = window.prompt(question.text)
+
+    if(useranswer === question.correctanswer){
+        userscore = userscore + 10
+    }
+
+}
+window.alert('your score is:' +userscore)
